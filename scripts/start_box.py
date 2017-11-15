dba05d6af8c36f81b2d9ea447961037c69ca2f6d
Volodymyr_Melnyk <Volodymyr_Melnyk@epam.com>
2017-11-15 17:25:37 +0200

    modified:   scripts/start_box.py

--- scripts/start_box.py
+++ scripts/start_box.py
@@ -1,7 +1,26 @@
 import boto3
 import os
-#use Amazon S2
-s2 = boto3.resource('s2')
-#Print out bucket names
-for bucket in s2.buckets.all():
-    print(bucket.apache2)
+from botocore.exceptions import ClientError
+
+# Create a Security Group and Rules
+##  http://boto3.readthedocs.io/en/latest/guide/ec2-example-security-group.html
+
+ec2 = boto3.client('ec2')
+response = ec2.describe_vpcs()
+vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
+
+try:
+    response = ec2.create_security_group(GroupName='SECURITY_GROUP_EC2_MIKRO',
+                                         Description='EC2_MIKRO_SG',
+                                         VpcId=vpc_id)
+    security_group_id = response['GroupId']
+    print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
+
+
+#Launching New Instances
+##  https://stackoverflow.com/questions/41518334/how-do-i-use-boto3-to-launch-an-ec2-instance-with-an-iam-role
+
+#ec2_instance = boto3.resource('ec2')
+#ec2_instance.create_instances(InstanceType='t2.micro',
+                              MinCount=1, MaxCount=1,
+                              SecurityGroupIds=[security_group_id],)
