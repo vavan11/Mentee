13f236ede4779dd87fd697ebe3d8c18386814ed9
Volodymyr_Melnyk <Volodymyr_Melnyk@epam.com>
2017-11-15 15:23:28 +0200

    modified:   scripts/python_pip_install_and_boto3.sh

--- scripts/python_pip_install_and_boto3.sh
+++ scripts/python_pip_install_and_boto3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+--------------------------------------------------------------------------------
 # Check Ubuntu
 if [[ $(cat /proc/version |grep -io "Ubuntu" | head -1 | wc -l) -eq 1 ]]
 then
@@ -14,7 +14,6 @@ then
     echo "install pip FAILED"
     exit 1
   fi
-
   pip install boto3
   #check boto3
   if [[ $(pip show boto3 |grep -io "boto3" |head -1) -eq boto3 ]]
@@ -25,8 +24,28 @@ then
     exit 1
   fi
 fi
+--------------------------------------------------------------------------------
 # Check Centos
 if [[ $(cat /proc/version |grep -io "Red" | head -1 |wc -l) -eq 1 ]]
 then
-  echo "red"
+  echo "\e[31mInstall_python-pip_for_Centos_and_boto3\e[0m"
+  yum -y update
+  yum -y install python-pip
+  #Check pip install
+  if [[ $(pip --version |grep -io "pip") -eq pip ]]
+  then
+    echo -e "\e[31mpip_install_OK\e[0m"
+  else
+    echo "install pip FAILED"
+    exit 1
+  fi
+  pip install boto3
+  #check boto3
+  if [[ $(pip show boto3 |grep -io "boto3" |head -1) -eq boto3 ]]
+  then
+    echo -e "\e[31mboto3_install_OK\e[0m"
+  else
+    echo "install boto3 FAILED"
+    exit 1
+  fi
 fi
