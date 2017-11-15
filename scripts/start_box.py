5d8531441757fcec90e7638d721317e186237683
Volodymyr_Melnyk <Volodymyr_Melnyk@epam.com>
2017-11-15 15:53:20 +0200

    modified:   scripts/start_box.py

--- scripts/start_box.py
+++ scripts/start_box.py
@@ -1,7 +1,7 @@
 import boto3
+import os
 #use Amazon S2
 s2 = boto3.resource('s2')
 #Print out bucket names
 for bucket in s2.buckets.all():
-    print(bucket.korobka)
-    
+    print(bucket.apache2)
