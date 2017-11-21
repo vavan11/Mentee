31cb999cfc65beb1e623e5d7f8f3ae56a2c97ff1
Volodymyr Melnyk <volodymyr_melnyk@epam.com>
2017-11-21 17:21:12 +0200

    add vagrant file, and provision ssh_key.sh, provision_ansible.sh

---
+++ scripts/provision_ssh_key.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir /home/vagrant/.ssh
+cd /home/vagrant/.ssh/
+cp /vagrant/ssh/id_rsa id_rsa
+chmod 400 id_rsa
+chown -R vagrant:vagrant /home/vagrant/.ssh/
+cp /vagrant/ssh/id_rsa.pub id_rsa.pub
+cat /vagrant/ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
