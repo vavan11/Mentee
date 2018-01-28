c476ca7416b493ecbd3747a967726e903ce5f984
vavan11 <vovamelnik@gmail.com>
2018-01-28 18:58:19 +0200

    Add files via upload

---
+++ scripts/python_for_centos.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yum -y install gcc wget
+wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+tar xzf Python-2.7.10.tgz
+cd Python-2.7.10 && ./configure && make altinstall
+yum -y install python-pip
